1c1265dffab6a1b4a4737d911c6964891f3b6da8
mn987 <73776095+mn987@users.noreply.github.com>
2024-04-08 18:33:38 +0700

    [27]-Update home.pug - Cuong

--- routers/device.route.js
+++ routers/device.route.js
@@ -1,18 +1,22 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
+const authenToken = require("../modules/authServer");
+const devicesController = require("../controllers/devicesController");
 
-const authenToken = require("../modules/authServer")
-const devicesController = require("../controllers/devicesController")
+// Define routes with HTTP methods and corresponding controller functions
+router.get('/create', devicesController.showCreateDevicePage);
+router.post('/add', devicesController.createDeviceDB);
+router.post('/update', devicesController.updateDeviceDB);
+router.post('/delete', devicesController.deleteDeviceDB);
+router.get('/report', devicesController.ShowReportDevicePage);
 
-router.get('/create', devicesController.createDevice)
-router.post('/add', devicesController.createDeviceDB)
-router.post('/update', devicesController.updateDeviceDB)
-router.post('/delete', devicesController.deleteDeviceDB)
-router.get('/report', devicesController.reportDevice)
-router.get('/loan', authenToken.authenToken, devicesController.loanDevice)
-router.post('/loan', authenToken.authenToken, devicesController.loanDeviceDB)
-router.get('/return', authenToken.authenToken, devicesController.returnDevice)
-router.post('/return', authenToken.authenToken, devicesController.returnDeviceDB)
-router.get('/loanrecord', devicesController.loanRecord)
+// Routes requiring authentication
+router.get('/loan', authenToken.authenToken, devicesController.loanDevice);
+router.post('/loan', authenToken.authenToken, devicesController.loanDeviceDB);
+router.get('/return', authenToken.authenToken, devicesController.returnDevice);
+router.post('/return', authenToken.authenToken, devicesController.returnDeviceDB);
+
+// Non-authenticated route
+router.get('/loanrecord', devicesController.loanRecord);
 
 module.exports = router;
